5468d529fa7ae2b6a937d9adb67c0da12c72babd
Tushar Saxena <mailmeastushar@gmail.com>
2023-12-14 20:56:43 +0530

    friends list api integrated

--- src/components/friends/index.js
+++ src/components/friends/index.js
@@ -1,39 +1,54 @@
-import React from 'react'
+import React,{ useEffect, useState }  from 'react'
 import { Avatar, List } from 'antd';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import NewExpense from '../misc/newExpense';
+import { fetchFriends } from '../service/account-service';
+import Loader from '../misc/loader';
+import NoData from '../misc/nodata';
 
-const data = [
-    {
-      userName: `Badal`,
-      balance: "INR 10,000",
-      type: "owes you"
-    },
-    {
-      userName: `Tushar`,
-      balance: "INR 10,000",
-      type: "you owe"
-    },
-    {
-      userName: `Kartikey`,
-      balance: "INR 10,000",
-      type:"you owe"
-    },
-  ];
+function Friends(props) {
 
-  function Friends(props) {
-    const SettleAmount = (user, amount) => {
-      console.log('called settled', user,amount)
-        for(let i=0; i<data.length; i++){
-            if(data[i].userName === user){
-                data[i].balance = ""
-                data[i].type = "Settled"
-            }
+  const [data, setUserData] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [nodata, setNodata] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchFriends();
+        console.log('datatatatat==>', data)
+        if (data){
+          setUserData(data);
+          setIsLoading(false);
         }
-    }
+        else{
+          // throw new Error('Fetching account details failed');
+          setNodata(true);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error('Fetching account details failed:', error);
+          setNodata(true);
+          setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const SettleAmount = (user, amount) => {
+    console.log('called settled', user,amount)
+      for(let i=0; i<data.length; i++){
+          if(data[i].userName === user){
+              data[i].balance = ""
+              data[i].type = "Settled"
+          }
+      }
+  }
   return (
+    isLoading ? <Loader /> 
+    : 
+      nodata ? <NoData /> :
     <Container>
         <Row>
             <h3>{props.title}</h3>
@@ -50,25 +65,25 @@ const data = [
             <List.Item>
                 <List.Item.Meta
                 avatar={<Avatar style={{width: '50px', height: '50px'}} src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
-                title={<span style={{fontSize: '16px'}}>{item.userName}</span>}
+                title={<span style={{fontSize: '16px'}}>{item.name[0].toUpperCase()+item.name.slice(1)}</span>}
                 // description = {<span style={{color: 'green'}}>{item.content}</span>}
                 />
                 <div >
-                        {
-                        item.type === "you owe" ?(
+                      {
+                      item.type === "you owe" ?(
+                          <>
+                            <span className='orange-clr' >{item.type}</span>
+                            <p className='orange-clr' >{item.balance}</p> 
+                            <button className='btn-orange-clr btn' onClick={()=>SettleAmount(item.userName, item.balance)} >Settle</button>
+                          </>
+                        ) : (
                             <>
-                              <span className='orange-clr' >{item.type}</span>
-                              <p className='orange-clr' >{item.balance}</p> 
-                              <button className='btn-orange-clr btn' onClick={()=>SettleAmount(item.userName, item.balance)} >Settle</button>
+                              <span className='green-clr' >{item.type}</span> 
+                              <p className='green-clr' >{item.balance}</p>
                             </>
-                          ) : (
-                              <>
-                                <span className='green-clr' >{item.type}</span> 
-                                <p className='green-clr' >{item.balance}</p>
-                              </>
-                          )
-                        }
-                    
+                        )
+                      }
+                  
                 </div>
             </List.Item>
             )}
